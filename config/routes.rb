e4576d91d8af572e7d299bd304f59c71e38bb35e
Hector Sanchez <sanchez.hector.m@gmail.com>
2025-07-01 21:15:19 -0400

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   # Health check endpoint
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
   # Root route for API info
-  root to: proc { [200, {}, [{ 
-    message: "SMS API is running", 
+  root to: proc { [200, {}, [{
+    message: "SMS API is running",
     version: "1.0.0",
     endpoints: {
       register: "POST /users",
-      login: "POST /auths", 
+      login: "POST /auths",
       logout: "DELETE /auths",
       send_sms: "POST /messages",
       get_messages: "GET /users/:user_id/messages"
     }
   }.to_json]] }
-  
+
   resources :users, only: [:create] do
     resources :messages, only: [:index]
   end
